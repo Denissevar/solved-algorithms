930e3ad760443f901f463c9f485eb5a86c8a311a
Denissevar <dvargasvel@gmail.com>
2025-03-31 18:02:02 +0200

    part1

--- 66-nickname-generator/definition.js
+++ 66-nickname-generator/definition.js
@@ -2,6 +2,19 @@
 
 function nicknameGenerator(name){
     /** Reescribe con tus palabras y con pseudocódigo qué hay que hacer aquí.  */
+
+//* - Comprobar la longitud del nickname
+//* Si tiene menos de 4 letras, saldra error
+//* - Si tiene +4 letras y:
+// 1. la tercera (indice [2]) es una vocal, se devuelven 4 letras
+// 2. la tercera letra es consonante, se devuelven 3 letras  
+// Necesito un array or string = string porque son letras 
+// Necesito devolver un resultado? Si (error, 3 letras o 4 letras) 
+// Necesito comparar : longitud del nickname y si la letra en posicion [2] es vocal
+// Nickname muy corto = error
+// Nickname +4 letras = ver la letra 3 posicion [2]
+// Es vocal? = si devuelvo 4 letras *//  
+
 }
 
 console.log(nicknameGenerator("Robert")); // "Rob"
